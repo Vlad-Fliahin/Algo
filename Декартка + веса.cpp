82291b484de9ce0c619baa82fcf66d868e0c7172
Vladosik <55523599+Vlad-Fliahin@users.noreply.github.com>
2019-12-18 15:00:38 +0200

    Update Декартка + веса.cpp
    Поправочка добавления веса при обновлении

--- Декартка + веса.cpp
+++ Декартка + веса.cpp
@@ -8,33 +8,33 @@ struct tree_node {
     tree_node *left, *right;
     int key, priority, size;
     
-    tree_node(int new_key, int new_size)
+    tree_node(int new_key)
     {
         key = new_key;
-        size = new_size;
+        size = 1;
         priority = rand() % INF;
         left = right = nullptr;
     }
 };
 
 int getsize(tree_node *root) {
-    return root == nullptr ? 1 : root->size;
+    return root == nullptr ? 0 : root->size;
 }
 
-void update(tree_node *root) {
+void update(tree_node *&root) {
     if (root == nullptr) return;
     root->size = getsize(root->left) + getsize(root->right) + 1;
 }
 
 int nth_elem(tree_node *root, int n) {
-    assert(root = nullptr);
-    int root_number = getsize(root);
+//    assert(root = nullptr);
+    int root_number = getsize(root->right) + 1;
     if (root_number == n)
         return root->key;
     if (root_number > n)
-        return nth_elem(root->left, n);
+        return nth_elem(root->right, n);
     else
-        return nth_elem(root->right, n - root_number - 1);
+        return nth_elem(root->left, n - root_number);
 }
 
 pair<tree_node*, tree_node*> split(tree_node *root, int key) {
@@ -44,12 +44,14 @@ pair<tree_node*, tree_node*> split(tree_node *root, int key) {
     {
         pair<tree_node*, tree_node*> splitted = split(root->right, key);
         root->right = splitted.first;
+        update(root);
         return make_pair(root, splitted.second);
     }
     else
     {
         pair<tree_node*, tree_node*> splitted = split(root->left, key);
         root->left = splitted.second;
+        update(root);
         return make_pair(splitted.first, root);
     }
 }
@@ -70,13 +72,13 @@ tree_node* merge(tree_node *left, tree_node *right) {
     }
 }
 
-void insert(tree_node *&root, int key, int value) {
+void insert(tree_node *&root, int key) {
     pair<tree_node*, tree_node*> splitted = split(root, key);
-    root = merge(merge(splitted.first, new tree_node(key, value)), splitted.second);
+    root = merge(merge(splitted.first, new tree_node(key)), splitted.second);
 }
 
 void erase(tree_node *&root, int key) {
-    assert(root == nullptr);
+//    assert(root == nullptr);
     if (root->key < key)
         erase(root->right, key);
     else if (root->key > key)
@@ -85,19 +87,23 @@ void erase(tree_node *&root, int key) {
         root = merge(root->left, root->right);
 }
 
-bool find(tree_node *root, int key) {
-    if (root == nullptr) return false;
-    if (root->key == key) return true;
-    if (root->key < key)
-        return find(root->right, key);
-    else
-        return find(root->left, key);
-}
-
 int main() {
     tree_node *node = nullptr;
-    int key;
+    int key, n;
     string s;
+    
+    cin >> n;
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> s >> key;
+        
+        if (s == "1" || s == "+1") {
+            insert(node, key);
+        } else if (s == "0") {
+            cout << nth_elem(node, key) << "\n";
+        } else
+            erase(node, key);
+    }
     
     return 0;
 }
